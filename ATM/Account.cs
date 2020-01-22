1e552e374688a8dccc320c6b849de23fa3d77a1f
libbyaudrey <libbyaudrey@gmail.com>
2020-01-22 12:09:55 -0500

    Initial check-in of module ATM

---
+++ ATM/Account.cs
@@ -0,0 +1,126 @@
+﻿//Libby Snedaker
+//October 13, 2019
+
+using System;
+namespace ATM
+{
+    //new class for Account
+    public class Account
+    {
+        //account object constructor
+        public Account(int accountNumberIn, int PINin, double balanceIN)
+        {
+            aNumber = accountNumberIn;
+            pin = PINin;
+            theBalance = balanceIN;
+        }
+
+        //Account number property
+        int accountNumber;
+        public int aNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+
+            set
+            {
+                //Account Number has to be 10 digits only
+                if (value.ToString().Length == 10)
+                {
+                    accountNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a 10 digit account number");
+                }
+
+            }
+        }
+
+        //PIN property
+        int PIN;
+        public int pin
+        {
+            get
+            {
+                return PIN;
+            }
+
+            set
+            {
+                //PIN is 4 digits only
+                if (value.ToString().Length == 4)
+                {
+
+                    PIN = value;
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a 4 digit PIN");
+                }
+
+            }
+        }
+
+        //Balance property
+        public double balance;
+        public double theBalance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                //Balance cannot be negative
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have enough money to withdraw that amount.");
+                }
+
+            }
+        }
+
+        //adds money to the balance
+        public void Deposit(double myDeposit)
+        {
+
+            balance = balance + myDeposit;
+
+            Console.WriteLine("Now your new balance is: " + balance);
+
+        }
+
+        //withdraws money from the balance
+        public void Withdrawal(double myWithdrawal)
+        {
+            Console.WriteLine("There is a 4% fee to withdraw your money from account: " + accountNumber + "." + " Do you still want to withdraw it? (please enter 'yes' or 'no'.) ");
+            string answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+                if (balance - (myWithdrawal*1.04) >= 0)
+                {
+                    balance = balance - (myWithdrawal*1.04);
+                    Console.WriteLine("Great, your new balance is: " + Math.Round(balance, 2));
+                }
+                else
+                {
+                    Console.WriteLine("You don't have enough money to make this withdrawal.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Okay, that's fine, your balance remains: " + balance);
+            }
+
+        }
+    }
+}
---
+++ ATM/Program.cs
@@ -0,0 +1,74 @@
+﻿//Libby Snedaker
+//October 13, 2019
+
+using System;
+
+namespace ATM
+{ 
+    class Program
+    {
+        //main method to run the user transactions
+        static void Main(string[] args)
+        {
+            //three accounts/three account objects w/hardcoded values
+            Account account1 = new Account(1001001001, 1234, 100.00);
+            Account account2 = new Account(1001001002, 5678, 20.25);
+            Account account3 = new Account(1001001003, 9010, 5.00);
+
+
+            //account 1 ask for PIN
+            Console.Write("Enter your PIN for account1: ");
+            account1.pin = int.Parse(Console.ReadLine());
+
+            //display account1 balance
+            Console.WriteLine("Hello, your balance for account1 is " + account1.theBalance);
+
+            //deposit money into account1
+            Console.Write("Enter amount to deposit into account1 " + account1.aNumber + ": ");
+            double myDeposit = double.Parse(Console.ReadLine());
+            account1.Deposit(myDeposit);
+
+            //withdraw money from account1
+            Console.Write("Enter amount to withdraw from account1 " + account1.aNumber + ": ");
+            double myWithdraw = double.Parse(Console.ReadLine());
+            account1.Withdrawal(myWithdraw);
+
+            //account 2 ask for PIN
+            Console.Write("Enter your PIN for account2: ");
+            account2.pin = int.Parse(Console.ReadLine());
+
+            //display account2 balance
+            Console.WriteLine("Hello, your balance for account2 is " + account2.theBalance);
+
+            //deposit money into account2
+            Console.Write("Enter amount to deposit into account2: " + account2.aNumber + ": ");
+            myDeposit = double.Parse(Console.ReadLine());
+            account2.Deposit(myDeposit);
+
+            //withdraw money from account2
+            Console.Write("Enter amount to withdraw from account2 " + account2.aNumber + ": ");
+            myWithdraw = double.Parse(Console.ReadLine());
+            account2.Withdrawal(myWithdraw);
+
+            //account 3 ask for PIN
+            Console.Write("Enter your PIN for account3: ");
+            account3.pin = int.Parse(Console.ReadLine());
+
+            //display account3 balance
+            Console.WriteLine("Hello, your balance for account3 is " + account3.theBalance);
+
+            //deposit money into account3
+            Console.Write("Enter amount to deposit into account3 " + account3.aNumber + ": ");
+            myDeposit = double.Parse(Console.ReadLine());
+            account3.Deposit(myDeposit);
+
+            //withdraw money from account3
+            Console.Write("Enter amount to withdraw from account3 " + account3.aNumber + ": ");
+            myWithdraw = double.Parse(Console.ReadLine());
+            account3.Withdrawal(myWithdraw);
+
+            //keeps program from ending automatically
+            Console.ReadKey();
+        }
+    }
+}
